d7342cb3b2f1e052c4887b6cced30b3ae9184aea
Ali Razzak <aliarazzak@hotmail.com>
2021-04-17 12:10:57 +1200

    first_question_done :japanese_goblin:

---
+++ stronghold/count_dna_nucleotide/counting_dna.py
@@ -0,0 +1,25 @@
+nucleotide_allowed = ["A", "T", "G", "C"]
+
+the_sequence_file = input("Name of file please: ")
+
+sequence_file = open(the_sequence_file, 'r')
+
+the_sequence = sequence_file.read()
+
+A_count = 0
+T_count = 0
+G_count = 0
+C_count = 0
+
+for nucleotide in the_sequence:
+	if nucleotide in nucleotide_allowed:
+		if nucleotide == "A":
+			A_count += 1
+		elif nucleotide == "T":
+			T_count += 1
+		elif nucleotide == "G":
+			G_count += 1
+		elif nucleotide == "C":
+			C_count += 1
+
+print(str(A_count) + " " + str(C_count) + " " + str(G_count) + " " + str(T_count))
