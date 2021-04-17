55121968681f3bf9c409edb031f7dd5faa7f369c
Ali Razzak <aliarazzak@hotmail.com>
2021-04-17 14:24:58 +1200

    fifth_challenge_done_:japanese_goblin:

---
+++ stronghold/computing_gc_content/computing_gc_content.py
@@ -0,0 +1,19 @@
+from Bio import SeqIO
+from Bio.SeqUtils import GC
+
+the_fasta_file = input("Name of file please: ")
+
+fasta_file = open(the_fasta_file, 'r')
+
+highest_desc = ""
+highest_GC = 0
+
+with open(the_fasta_file) as fasta_file:
+	for record in SeqIO.parse(fasta_file, "fasta"):
+		if GC(record.seq) > highest_GC:
+			highest_desc = record.id
+			highest_GC = GC(record.seq)
+
+
+print(highest_desc)
+print(highest_GC)
