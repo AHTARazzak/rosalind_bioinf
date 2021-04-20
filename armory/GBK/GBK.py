75d1de3798b71bff7ecd419d460a732ca3091667
Ali Razzak <aliarazzak@hotmail.com>
2021-04-20 20:17:33 +1200

    Did rosalind GBK

--- armory/GBK/GBK.py
+++ armory/GBK/GBK.py
@@ -1,3 +1,11 @@
+'''
+My solution to Rosalind Bioinformatics Problem GBK
+Title: Introduction to the Bioinformatics Armory
+Rosalind ID: GBK
+http://rosalind.info/problems/gbk/
+Goal: The number of Nucleotide GenBank entries for the given genus that were published between the dates specified.
+'''
+
 from Bio import Entrez
 import datetime
 import sys
@@ -17,3 +25,5 @@ the_id_list = record["IdList"]
 for i in the_id_list:
 	handle = Entrez.efetch(db="nucleotide", id=i, rettype="gb", retmode="text")
 	print(handle.readline().strip())
+
+#Ali Razzak
