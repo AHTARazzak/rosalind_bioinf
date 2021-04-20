656386159f01b81630ac7112c2da51423a5e479b
Ali Razzak <aliarazzak@hotmail.com>
2021-04-20 16:01:23 +1200

    Another one :japanese_goblin:

---
+++ stronghold/finding_a_spliced_motif/finding_a_spliced_motif.py
@@ -0,0 +1,24 @@
+from Bio import SeqIO, Seq
+import re
+import sys
+
+sequence_list = []
+
+with open(sys.argv[1]) as fasta_file:
+	for record in SeqIO.parse(fasta_file, "fasta"):
+		sequence_list.append(str(record.seq))
+
+big_sequence = sequence_list[0].split()[0]
+subseq = sequence_list[1].split()[0]
+
+find_list = []
+
+make_up = 0
+
+for nt in subseq:
+	this_spot = big_sequence.find(nt)
+	find_list.append(str(make_up + this_spot + 1))
+	make_up += this_spot + 2
+	big_sequence = big_sequence[this_spot + 2:]
+
+print(" ".join(find_list))
