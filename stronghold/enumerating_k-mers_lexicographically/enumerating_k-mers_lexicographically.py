cc48de64e00e5926797c4f7832c38d2ec0a7e427
Ali Razzak <aliarazzak@hotmail.com>
2021-04-20 13:22:32 +1200

    Another one :japanese_gobin:

---
+++ stronghold/enumerating_k-mers_lexicographically/enumerating_k-mers_lexicographically.py
@@ -0,0 +1,39 @@
+import sys
+
+data_file = open(sys.argv[1]).readlines()
+symbols = data_file[0].split('\n')[0]
+symbols_val = symbols.split()
+length = data_file[1].split('\n')[0]
+
+combin_num = len(symbols_val)**int(length)
+list_of_combos = []
+the_crunch = ([1] * int(length))
+
+for i in range(combin_num):
+	for column in range(len(the_crunch)):
+		if (the_crunch[column] > len(symbols_val)):
+			the_crunch[column + 1] += 1
+			the_crunch[column] = 1
+	list_of_combos += the_crunch
+	the_crunch[0] += 1
+
+list_of_combos = [list_of_combos[i:i + int(length)] for i in range(0, len(list_of_combos), int(length))]
+
+convert_combos = []
+
+for entry in list_of_combos:
+	this_combo = []
+	for element in entry:
+		this_combo.append(symbols_val[element - 1])
+	convert_combos.append("".join(this_combo))
+
+STRING_LIST = []
+for element in convert_combos:
+	STRING_LIST.append("".join(element))
+
+STRING_LIST.sort()
+print(STRING_LIST)
+
+outfile = open("submit_this.txt", "w")
+
+outfile.write("\n".join(STRING_LIST))
