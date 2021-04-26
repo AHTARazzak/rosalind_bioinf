36b277b5974a04c4eb3725f891167ed34b933f86
Ali Razzak <aliarazzak@hotmail.com>
2021-04-26 16:48:16 +1200

    Solved CLUS

---
+++ armory/CLUS/CLUS.py
@@ -0,0 +1,13 @@
+'''
+My solution to Rosalind Bioinformatics Problem CLUS
+Title: Global Multiple Alignment
+Rosalind ID: CLUS
+http://rosalind.info/problems/clus/
+Goal: ID of the string most different from the others.
+'''
+import os
+import sys
+
+os.system("clustalo --outfmt=fa --output-order=tree-order --force -i " + sys.argv[1] + " -o CLUS.output")
+
+print(open("CLUS.output").readlines()[0][1:])
