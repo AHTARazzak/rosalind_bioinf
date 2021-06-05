9e1b33bf15d2dfbda81ee48aacde0595fc3244f5
Ali Razzak <aliarazzak@hotmail.com>
2021-06-05 22:40:55 +1200

    Solved GCON

--- stronghold/GCON/GCON.py
+++ stronghold/GCON/GCON.py
@@ -4,37 +4,86 @@ Rosalind ID: GCON
 URL: http://rosalind.info/problems/gcon
 Goal: The maximum alignment score between s and t. Use:
 '''
+import urllib
+import contextlib
 import sys
-from itertools import product
-from Bio.SubsMat.MatrixInfo import blosum62
+import os
+from numpy import zeros
 
-def get_score(s, t):
-    sl, tl = len(s), len(t)
-    m = {(0, 0): (0, None)}
-    f, g, h = {}, {}, {}
-    
-    m.update({((i, 0), (-5, (i - 1, 0))) for i in range(1, sl + 1)})
-    m.update({((0, i), (-5, (0, i - 1))) for i in range(1, tl + 1)})
-    
-    for i, j in product(range(1, sl + 1), range(1, tl + 1)):
-        cost = blosum62.get((s[i - 1], t[j - 1]))
+class BLOSUM62(object):
+    def __init__(self):
+        with open(os.path.join(os.path.dirname(__file__), 'BLOSUM62.txt')) as input_data:
+            items = [line.strip().split() for line in input_data.readlines()]
+            self.scoring_matrix = {(item[0], item[1]):int(item[2]) for item in items}
+
+    def __getitem__(self, pair):
+        return self.scoring_matrix[pair[0], pair[1]]
+
+def ReadFASTA(data_location):
+        if type(data_location) == list:
+                fasta_list = list()
+                for location in data_location:
+                        fasta_list+=ReadFASTA(location)
+                return fasta_list
+
+        if data_location[-4:] == '.txt':
+                with open(data_location) as f:
+                        return ParseFASTA(f)
         
-        if cost == None:
-            cost = blosum62.get((t[j - 1], s[i - 1]))
-        f[(i, j)] = m[(i - 1, j - 1)][0] + cost
-        g[(i, j)] = max(m[(i - 1, j)][0] - 5, g.get((i - 1, j)))
-        h[(i, j)] = max(m[(i, j - 1)][0] - 5, h.get((i, j - 1)))
-        v = max(f[(i, j)], g[(i, j)], h[(i, j)])
-        if v == f[(i, j)]:
-            m[(i, j)] = (v, (i-1, j - 1))
-        elif v == g[(i, j)]:
-            m[(i, j)] = (v, (i - 1, j))
-        elif v == h[(i, j)]:
-            m[(i, j)] = (v, (i, j - 1))
-    return m[(i,j)][0]
-    
-if __name__ == '__main__':
-    dataset = open(sys.argv[1]).read().strip().split('\n')
+        elif data_location[0:4] == 'http':
+                with contextlib.closing(urllib.urlopen(data_location)) as f:
+                        return ParseFASTA(f)
 
-    print (get_score(dataset[1], dataset[3]))
+
+def ParseFASTA(f):
+        fasta_list = list()
+        for line in f:
+                if line[0] == '>':
+                        try:
+                                fasta_list.append(current_dna)
+                        except UnboundLocalError:
+                                pass
+                        current_dna = [line.lstrip('>').rstrip('\n'),'']
+
+                else:
+                        current_dna[1] += line.rstrip('\n')
+        
+        fasta_list.append(current_dna)
+
+        return fasta_list
+
+def global_alignment_with_constant_gap(v, w, scoring_matrix, sigma):
+
+    S_lower = zeros((len(v)+1, len(w)+1), dtype=int)
+    S_middle = zeros((len(v)+1, len(w)+1), dtype=int)
+    S_upper = zeros((len(v)+1, len(w)+1), dtype=int)
+
+    for i in range(1, len(v)+1):
+        S_lower[i][0] = -sigma
+        S_middle[i][0] = -sigma
+        S_upper[i][0] = -10*sigma
+    for j in range(1, len(w)+1):
+        S_upper[0][j] = -sigma
+        S_middle[0][j] = -sigma
+        S_lower[0][j] = -10*sigma
+
+    for i in range(1, len(v)+1):
+        for j in range(1, len(w)+1):
+            S_lower[i][j] = max([S_lower[i-1][j], S_middle[i-1][j] - sigma])
+            S_upper[i][j] = max([S_upper[i][j-1], S_middle[i][j-1] - sigma])
+            S_middle[i][j] = max([S_lower[i][j], S_middle[i-1][j-1] + scoring_matrix[v[i-1], w[j-1]], S_upper[i][j]])
+
+    max_score = S_middle[len(v)][len(w)]
+
+    return max_score
+
+if __name__ == '__main__':
+    s, t = [fasta[1] for fasta in ReadFASTA(sys.argv[1])]
+
+    score = str(global_alignment_with_constant_gap(s, t, BLOSUM62(), 5))
+
+    print (score)
+    with open('submit.txt', 'w') as output_data:
+        output_data.write(score)
+        
 #Ali Razzak
